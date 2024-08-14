6cd792cbfa7acb26732da42c51d01c26287a4577
UsaidRashid <rashidusaid2@gmail.com>
2024-08-14 14:57:19 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,7 @@ import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
-import Home from "./Components/Home";
 import UpcomingExams from "./Components/UpcomingExams";
-import Exams from "./Components/Exams";
 import DisplayPaper from "./Components/DisplayPaper";
 import Navbar from "./Components/Navbar";
 import TeacherDashboard from "./Components/TeacherDashboard";
@@ -13,7 +11,11 @@ import StudentDashboard from "./Components/StudentDashboard";
 const AppContent = () => {
   const location = useLocation();
   let showNavbar = true;
-  if (location.pathname === "/login" || location.pathname === "/signup")
+  if (
+    location.pathname === "/login" ||
+    location.pathname === "/signup" ||
+    location.pathname === "/display-paper"
+  )
     showNavbar = false;
   return (
     <>
@@ -21,12 +23,10 @@ const AppContent = () => {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
         <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/display-paper" element={<DisplayPaper />} />
         <Route path="/upcoming-exams" element={<UpcomingExams />} />
-        <Route path="/exams" element={<Exams />} />
       </Routes>
     </>
   );
