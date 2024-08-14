f6b8d8f7b2dbe988e599dd0385c891f093a7354e
UsaidRashid <rashidusaid2@gmail.com>
2024-08-14 18:39:42 +0530

    All Set For Rendering

--- backend/ai_module/AI.js
+++ backend/ai_module/AI.js
@@ -1,36 +1,35 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const Tesseract = require('tesseract.js');
+const Tesseract = require("tesseract.js");
 
 async function extractTextFromImage(imagePath) {
-    try {
-        const text = await Tesseract.recognize(imagePath, 'eng'); 
-        return text.data.text;
-    } catch (error) {
-        console.error("Error extracting text from image:", error);
-        return null;
-    }
+  try {
+    const text = await Tesseract.recognize(imagePath, "eng");
+    return text.data.text;
+  } catch (error) {
+    console.error("Error extracting text from image:", error);
+    return null;
+  }
 }
-const apiKey=process.env.GEMINI_API_KEY;
-// console.log(apiKey)
+const apiKey = process.env.GEMINI_API_KEY;
 
 async function generateRandomQuestions(syllabusPath, numberOfQuestions) {
-    console.log('in AI');
-    const syllabusText = await extractTextFromImage(syllabusPath);
-    const sampleQuestion = "**Q. A sample Question#\n**A. option A#\n**B. option B#\n**C. option C#\n**D. option D#\n**Ans. A/B/C/D#";
+  const syllabusText = await extractTextFromImage(syllabusPath);
+  const sampleQuestion =
+    "**Q. A sample Question#\n**A. option A#\n**B. option B#\n**C. option C#\n**D. option D#\n**Ans. A/B/C/D#";
 
-    const genAI = new GoogleGenerativeAI(apiKey);
-    
-    const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  const genAI = new GoogleGenerativeAI(apiKey);
 
-    const prompt = `Generate a random Multiple Choice Question paper of ${numberOfQuestions} questions with 4 options A,B,C,D each based on the given syllabus:\n\n${syllabusText} \n\n The format of the MCQs should be like this \n Each Question should start with letter '**Q.' and each option A,B,C,D should be in separate lines,and at the last line should be the correct option i.e the answer of the questions, also '#' should be used to mark the end of question and each option like this : \n ${sampleQuestion}`;
-    
-    try {
-      const result = await model.generateContent(prompt);
-      const response = result.response;
-      return response.text();
-    } catch (error) {
-        console.error("Error generating questions:", error);
-    }
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = `Generate a random Multiple Choice Question paper of ${numberOfQuestions} questions with 4 options A,B,C,D each based on the given syllabus:\n\n${syllabusText} \n\n The format of the MCQs should be like this \n Each Question should start with letter '**Q.' and each option A,B,C,D should be in separate lines,and at the last line should be the correct option i.e the answer of the questions, also '#' should be used to mark the end of question and each option like this : \n ${sampleQuestion}`;
+
+  try {
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    return response.text();
+  } catch (error) {
+    console.error("Error generating questions:", error);
   }
+}
 // generateRandomQuestions()
 module.exports = generateRandomQuestions;
--- frontend/src/Components/TeacherDashboard.jsx
+++ frontend/src/Components/TeacherDashboard.jsx
@@ -12,6 +12,7 @@ export default function TeacherDashboard() {
     timeAllotted: 0,
     syllabusImage: null,
   });
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
 
@@ -34,6 +35,7 @@ export default function TeacherDashboard() {
   const postData = async (e) => {
     e.preventDefault();
 
+    setLoading(true);
     const formData = new FormData();
     formData.append("name", paper.name);
     formData.append("numQuestions", paper.numQuestions);
@@ -42,11 +44,15 @@ export default function TeacherDashboard() {
     formData.append("syllabusImage", paper.syllabusImage);
 
     try {
-      const response = await axios.post(api + "exams/generate-questions", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const response = await axios.post(
+        api + "exams/generate-questions",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
       if (response.status === 200) {
         alert("Paper Generated Successfully!");
         window.location.reload();
@@ -65,133 +71,141 @@ export default function TeacherDashboard() {
       } else {
         alert("Error setting up the request: " + error.message);
       }
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="bg-dashboard bg-cover bg-center min-h-screen flex">
       <Sidebar />
-      <div className="flex-grow p-8">
-        <div className="max-w-4xl mx-auto bg-white/80 backdrop-blur-lg rounded-lg shadow-lg p-6">
-          <h1 className="text-3xl font-bold text-gray-700 text-center mb-8">
-            Create Exam Paper
-          </h1>
-          <form onSubmit={postData}>
-            <div className="space-y-6">
-              <div className="flex items-center">
-                <label
-                  htmlFor="Name"
-                  className="w-44 text-center text-lg font-semibold text-gray-700"
-                >
-                  Subject:
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  value={paper.name}
-                  onChange={handleChange}
-                  className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  id="Name"
-                />
-              </div>
-              <div className="flex items-center">
-                <label
-                  htmlFor="numQuestions"
-                  className="w-44 text-center text-lg font-semibold text-gray-700"
-                >
-                  Questions:
-                </label>
-                <input
-                  type="number"
-                  name="numQuestions"
-                  value={paper.numQuestions}
-                  onChange={handleChange}
-                  className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  id="numQuestions"
-                />
-              </div>
-              <div className="flex items-center">
-                <label
-                  htmlFor="scheduledTime"
-                  className="w-44 text-center text-lg font-semibold text-gray-700"
-                >
-                  Starting Time:
-                </label>
-                <input
-                  type="datetime-local"
-                  name="scheduledTime"
-                  value={paper.scheduledTime}
-                  onChange={handleChange}
-                  className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  id="scheduledTime"
-                />
-              </div>
-              <div className="flex items-center">
-                <label
-                  htmlFor="timeAllotted"
-                  className="w-44 text-center text-lg font-semibold text-gray-700"
-                >
-                  Allotted Time:
-                </label>
-                <input
-                  type="number"
-                  name="timeAllotted"
-                  value={paper.timeAllotted}
-                  onChange={handleChange}
-                  className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                  id="timeAllotted"
-                />
-              </div>
-              <div className="flex items-center justify-between">
-                <div className="flex items-center justify-center w-64">
+      {loading ? (
+        <p className="text-lg font-semibold text-gray-600 animate-pulse">
+          Loading... Generating Exam, please wait.
+        </p>
+      ) : (
+        <div className="flex-grow p-8">
+          <div className="max-w-4xl mx-auto bg-white/80 backdrop-blur-lg rounded-lg shadow-lg p-6">
+            <h1 className="text-3xl font-bold text-gray-700 text-center mb-8">
+              Create Exam Paper
+            </h1>
+            <form onSubmit={postData}>
+              <div className="space-y-6">
+                <div className="flex items-center">
                   <label
-                    htmlFor="dropzone-file"
-                    className="flex flex-col items-center justify-center w-full h-28 border-2 border-dashed border-indigo-300 rounded-lg cursor-pointer transition hover:bg-indigo-50"
+                    htmlFor="Name"
+                    className="w-44 text-center text-lg font-semibold text-gray-700"
                   >
-                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                      <svg
-                        className="w-10 h-10 mb-4 text-indigo-500"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 20 16"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                        />
-                      </svg>
-                      <p className="mb-2 text-sm text-gray-500">
-                        <span className="font-semibold">Click to upload</span>{" "}
-                        or drag and drop
-                      </p>
-                      <p className="text-xs text-gray-500">
-                        SVG, PNG, JPG, or GIF (MAX. 800x400px)
-                      </p>
-                    </div>
-                    <input
-                      id="dropzone-file"
-                      type="file"
-                      className="hidden"
-                      name="syllabusImage"
-                      onChange={getFile}
-                    />
+                    Subject:
                   </label>
+                  <input
+                    type="text"
+                    name="name"
+                    value={paper.name}
+                    onChange={handleChange}
+                    className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    id="Name"
+                  />
                 </div>
-                <button
-                  type="submit"
-                  className="bg-indigo-500 text-white rounded-full py-3 px-8 transition hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Generate
-                </button>
+                <div className="flex items-center">
+                  <label
+                    htmlFor="numQuestions"
+                    className="w-44 text-center text-lg font-semibold text-gray-700"
+                  >
+                    Questions:
+                  </label>
+                  <input
+                    type="number"
+                    name="numQuestions"
+                    value={paper.numQuestions}
+                    onChange={handleChange}
+                    className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    id="numQuestions"
+                  />
+                </div>
+                <div className="flex items-center">
+                  <label
+                    htmlFor="scheduledTime"
+                    className="w-44 text-center text-lg font-semibold text-gray-700"
+                  >
+                    Starting Time:
+                  </label>
+                  <input
+                    type="datetime-local"
+                    name="scheduledTime"
+                    value={paper.scheduledTime}
+                    onChange={handleChange}
+                    className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    id="scheduledTime"
+                  />
+                </div>
+                <div className="flex items-center">
+                  <label
+                    htmlFor="timeAllotted"
+                    className="w-44 text-center text-lg font-semibold text-gray-700"
+                  >
+                    Allotted Time:
+                  </label>
+                  <input
+                    type="number"
+                    name="timeAllotted"
+                    value={paper.timeAllotted}
+                    onChange={handleChange}
+                    className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                    id="timeAllotted"
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center justify-center w-64">
+                    <label
+                      htmlFor="dropzone-file"
+                      className="flex flex-col items-center justify-center w-full h-28 border-2 border-dashed border-indigo-300 rounded-lg cursor-pointer transition hover:bg-indigo-50"
+                    >
+                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg
+                          className="w-10 h-10 mb-4 text-indigo-500"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 20 16"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                          />
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500">
+                          <span className="font-semibold">Click to upload</span>{" "}
+                          or drag and drop
+                        </p>
+                        <p className="text-xs text-gray-500">
+                          SVG, PNG, JPG, or GIF (MAX. 800x400px)
+                        </p>
+                      </div>
+                      <input
+                        id="dropzone-file"
+                        type="file"
+                        className="hidden"
+                        name="syllabusImage"
+                        onChange={getFile}
+                      />
+                    </label>
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-indigo-500 text-white rounded-full py-3 px-8 transition hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Generate
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
